fd0210ceeedaf4c0a851aa293f52fa3f8c96f208
Harsh Goyal <harshgoyal@HARSHs-MacBook-Air.local>
2023-12-19 23:07:22 +0530

    changed Link to home page from logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,9 @@ import logo from "../Assets/logo.png"
 const Header = () =>{
     return (
        <HStack p={"4"} shadow={"md"} bgColor={"blackAlpha.900"} >
+        <Link to="/">
             <Image src={logo} w={"10"} h={"10"} objectFit={"contain"} alt={"logo"} mx={"4"}/>
+        </Link>
             <Button variant={"unstyled"} color={"white"}>
                 <Link to={"/"}>Home</Link>
             </Button>
